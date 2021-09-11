04e583de71cede65a40430849293ff452d2eeddf
gonzag <gonzaguerrerotm@gmail.com>
2021-09-11 20:20:05 -0300

    agrego articulos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <CardComponent text={"Elemento 1"} img={"https://lh3.googleusercontent.com/proxy/bxKVr9bGbb0XYK0-4Qxdfd7FK1nZED3ju4iSn8ziVtMOeTlNyG6Ux1DU8u-azJncONGE-qAdhtOTl5awlXAR2qo0qzM3MDW3Hx7l1Y0DgNUs7REw7gY"}/>
-      <CardComponent text={"Elemento 2"} img={"https://lh3.googleusercontent.com/proxy/HxDeW4u-bfHU7_-LBiVmagzFBNCozzNSzvmbCnjc6MG_kVEVf2wgGmqPQTc7SL_BQZsAHt_PXkTBgB5wQ98edTMgJEO-619VGchsnLQpRY4p4YfcvIc"}/>
+      <CardComponent text={"Elemento 1"} img={"https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/adidas-x-Beavis-%26-Butthead-Skate-Cornholio-camiseta-blanca-_317726-front-US.jpg"}/>
+      <CardComponent text={"Elemento 2"} img={"https://m.media-amazon.com/images/I/613Bx2I71FL._AC_UX466_.jpg"}/>
       <CardComponent text={"Elemento 3"} img={"https://ferreira.vteximg.com.br/arquivos/ids/359471-1000-1000/ch_ichgt78h007.jpg?v=637403536767770000"}/>
     </div>
   );
